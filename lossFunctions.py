37e6d566fd09359f372c8d0fbdc744f2455f5367
Christoffer Riis <c@riisfam.dk>
2020-11-08 16:48:53 +0100

    update of gaussian density

--- lossFunctions.py
+++ lossFunctions.py
@@ -6,38 +6,40 @@ import torch
 def Gaussian_density(sample_img,mu_img,log_var_img):
     """
     Log likelihood of a Gaussian density (http://jrmeyer.github.io/machinelearning/2017/08/18/mle.html):
-    sum_1^n [ -0.5 * log(2π) - log(var)/2 - (x - mean)^2 / (2*var) ]
+    sum_1^n [ -0.5 * log(2π) - log(var)/2 - (x - mean)^2 / (2*var + eps) ]
 
     All inputs are w/ dimensions: ? [batch_size, channels, width, height]
 
     :param sample_img: sample x
     :param mu_img: mean
     :param log_var_img: logarithm of the variance
-    :return: log likelihood averaged across all batches and pixels
+    :return: log likelihood averaged across channels and pixels, output dim [batch_size]
     """
     c = - 0.5 * math.log(2 * math.pi)
-    log_likelihood = c - log_var_img/2 - (sample_img - mu_img)**2/(2 * torch.exp(log_var_img))
-    #log_likelihood = c -  (sample_img - mu_img)**2/(2 * torch.exp(log_var_img))
-    #print("log_likelihood:",log_likelihood)
-    #print("log_likelihood.shape:", log_likelihood.shape)
-    log_likelihood = torch.mean(log_likelihood, dim=1)  # Sum over channels
-    log_likelihood = log_likelihood.view(batch_size, -1)
-    log_likelihood = torch.mean(log_likelihood, dim=1) # Sum over features (224x224 = 50,176)
+    log_likelihood = c - log_var_img/2 - (sample_img - mu_img)**2/(2 * torch.exp(log_var_img + 1e-9))
+    log_likelihood = torch.mean(log_likelihood, dim=(1, 2, 3))
     return log_likelihood
 
+
 def kl_a_calc(q_a, q_mu, q_log_var,p_mu, p_log_var):
-    # The function assumes: 
-        # q_a has dimension: [batch_size,No_samples,latent_features]
-        # q_mu/log_var has dimension: [batch_size,latent_features]
-        # p_mu/log_var has dimension: [batch_size,No_samples,latent_features]
+    """
+    Kullback-Liebler divergence for auxiliary variable
+
+    :param q_a: [batch_size, no_samples, latent_features]
+    :param q_mu: [batch_size, latent_features]
+    :param q_log_var: [batch_size, latent_features]
+    :param p_mu: [batch_size, no_samples, latent_features]
+    :param p_log_var: [batch_size, no_samples, latent_features]
+    :return:
+    """
     
     def log_gaussian(x, mu, log_var):
         log_pdf = - 0.5 * math.log(2 * math.pi) - log_var / 2 - (x - mu)**2 / (2 * torch.exp(log_var))
-        log_pdf = torch.sum(log_pdf, dim=1) # sum over each over the observations (mu + log_var*epsilon)
-        log_pdf = torch.sum(log_pdf,dim=1) # sum over q_a, i.e. latent features
+        log_pdf = torch.sum(log_pdf, dim=1)  # sum over each over the observations (mu + log_var*epsilon)
+        log_pdf = torch.sum(log_pdf, dim=1)  # sum over q_a, i.e. latent features
         return log_pdf
 
-    # put in middle dimension
+    # put in middle dimension, i.e. no_samples
     q_mu      = q_mu.unsqueeze(1)
     q_log_var = q_log_var.unsqueeze(1)
     p_mu      = p_mu.unsqueeze_(1)
@@ -72,8 +74,6 @@ def ELBO_loss(sample_img, outputs, y = None, kl_warmup = None):
         a_log_var = torch.chunk(outputs['p_a_log_var'],num_classes,dim=1)
         for j in range(num_classes):
             likelihood = Gaussian_density(sample_img, torch.mean(x_mean[j],dim = 1), torch.mean(x_log_var[j],dim = 1))
-            # likelihood = likelihood.view(batch_size, -1)
-            # likelihood = torch.mean(likelihood, dim=1) # Sum over features (224x224 = 50,176)
             if aux_variables > 0:
                 kl_a = kl_a_calc(outputs["q_a"],outputs["q_a_mu"],outputs["q_a_log_var"],torch.mean(a_mean[j],dim = 1),torch.mean(a_log_var[j],dim = 1))
                 kl = w1 * kl_x + (1 - w1) * kl_a
@@ -95,8 +95,6 @@ def ELBO_loss(sample_img, outputs, y = None, kl_warmup = None):
         return -torch.mean(U), -torch.mean(H), -torch.mean(L),  (1 - w2) * beta * torch.mean(kl), -w2 * torch.mean(likelihood), w1*torch.mean(kl_x), (1-w1)*torch.mean(kl_a)
     else:
         likelihood = Gaussian_density(sample_img, torch.mean(outputs['x_mean'],dim = 1), torch.mean(outputs['x_log_var'],dim = 1))
-        # likelihood = likelihood.view(batch_size, -1)
-        # likelihood = torch.mean(likelihood, dim=1) # Sum over features (224x224 = 50,176)
         kl_x = -0.5 * torch.sum(1 + outputs['log_var'] - outputs['mu']**2 - torch.exp(outputs['log_var']), dim=1)
         kl_x = torch.sum(kl_x,dim=1) # sum over the features
         if aux_variables > 0:
